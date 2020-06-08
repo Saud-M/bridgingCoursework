9aa6346407c433d6a67509f2a65b1d955562a0b7
Saud Almbdal <saa718@cs.bham.ac.uk>
2020-06-08 15:05:14 +0300

    Welcome page

--- coursework/base/urls.py
+++ coursework/base/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
 
-    path('', views.home),
+    path('home/', views.home),
+    path('', views.temp),
     
 ]
--- coursework/base/views.py
+++ coursework/base/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'base/home.html')
+
+def temp(request):
+    return render(request, 'base/temp.html')
