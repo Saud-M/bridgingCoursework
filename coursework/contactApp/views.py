a2e2f9e1e7c292cfb83768349ecc56d65f831425
Saud Almbdal <saa718@cs.bham.ac.uk>
2020-06-18 06:20:06 +0300

    contactApp is ready

--- coursework/contactApp/views.py
+++ coursework/contactApp/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 
+from .forms import ContactForm
 # Create your views here.
-
+'''
 def contact(request, *args, **kwargs):
-	return render(request, 'contactApp/contact.html', {})
+    return render(request, 'contactApp/contact.html', {})
+'''
+def contact(request, *args, **kwargs):
+    if request.method =='POST':
+        form = ContactForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+    else:
+        form = ContactForm()
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'contactApp/contact.html', context)
